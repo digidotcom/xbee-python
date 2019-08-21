6ec2d95e226307c37624d73e68f918168e884e66
David Escalona <david.escalona@digi.com>
2019-08-21 18:37:38 +0200

    examples: filesystem: add new example to list XBee device directory contents
    https://jira.digi.com/browse/XBPL-217
    
    Signed-off-by: David Escalona <david.escalona@digi.com>

---
+++ examples/filesystem/ListDirectorySample/ListDirectorySample.py
@@ -0,0 +1,63 @@
+# Copyright 2019, Digi International Inc.
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from digi.xbee.filesystem import LocalXBeeFileSystemManager, FileSystemElement, FileSystemException
+from digi.xbee.devices import XBeeDevice
+from digi.xbee.exception import XBeeException
+
+# TODO: Replace with the serial port where your local module is connected to. 
+PORT = "COM1"
+# TODO: Replace with the baud rate of your local module.
+BAUD_RATE = 9600
+# TODO: Replace with the XBee file system path to list its contents. Leave as 'None' to use current dir.
+PATH_TO_LIST = None
+
+
+def main():
+    print(" +-------------------------------------------+")
+    print(" | XBee Python Library List Directory Sample |")
+    print(" +-------------------------------------------+\n")
+
+    device = XBeeDevice(PORT, BAUD_RATE)
+    filesystem_manager = LocalXBeeFileSystemManager(device)
+
+    try:
+        device.open()
+        filesystem_manager = LocalXBeeFileSystemManager(device)
+        print("Starting file system manager...", end=" ")
+        filesystem_manager.connect()
+        print("OK\n")
+        current_directory = filesystem_manager.get_current_directory()
+        print("Current directory: %s" % current_directory)
+        path_to_list = PATH_TO_LIST
+        if path_to_list is None:
+            path_to_list = current_directory
+        files = filesystem_manager.list_directory(path_to_list)
+        print("Contents of '%s':" % path_to_list)
+        for file in files:
+            print(str(file))
+    except (XBeeException, FileSystemException) as e:
+        print("ERROR: %s" % str(e))
+        exit(1)
+    finally:
+        if filesystem_manager.is_connected:
+            print("\nStopping file system manager...", end=" ")
+            filesystem_manager.disconnect()
+            print("OK")
+        if device is not None and device.is_open():
+            device.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/filesystem/ListDirectorySample/readme.txt
@@ -0,0 +1,69 @@
+  Introduction
+  ------------
+  This sample Python application shows how to list the contents of an XBee
+  device filesystem directory.
+  
+  The application uses the LocalXBeeFileSystemManager to access the device
+  filesystem and execute the required actions.
+  
+  NOTE: This example uses the generic XBee device (XBeeDevice) class, but it 
+        can be applied to any other local device class.
+
+
+  Requirements
+  ------------
+  To run this example you will need:
+  
+    * One XBee radio in API mode and its corresponding carrier board (XBIB 
+      or XBee Development Board).
+    * The XCTU application (available at www.digi.com/xctu).
+
+
+  Compatible hardware
+  --------------------
+    * Local XBee3 devices
+
+
+  Compatible protocols
+  --------------------
+    * 802.15.4
+    * DigiMesh
+    * ZigBee
+
+
+  Example setup
+  -------------
+    1) Plug the XBee radio into the XBee adapter and connect it to your
+       computer's USB or serial port.
+       
+    2) Ensure that the module is in API mode.
+       For further information on how to perform this task, read the 
+       'Configuring Your XBee Modules' topic of the Getting Started guide.
+       
+    3) Set the port and baud rate of the XBee radio in the sample file class.
+       If you configured the module in the previous step with the XCTU, you 
+       will see the port number and baud rate in the 'Port' label of the device 
+       on the left view.
+
+    4) Configure the path of the XBee directory to list.
+
+
+  Running the example
+  -------------------
+  First, build and launch the application. To test the functionality, check
+  that the console lists the selected directory contents like this:
+  
+    Starting file system manager... OK
+
+    Current directory: /flash
+    Contents of '/flash':
+    <DIR> ./
+    <DIR> ../
+    <DIR> lib/
+    <DIR> folder_1/
+    <DIR> folder_2/
+    42664 firmware.py
+
+    Stopping file system manager... OK
+
+  If any error occurs during the process, it will be displayed in the console.
