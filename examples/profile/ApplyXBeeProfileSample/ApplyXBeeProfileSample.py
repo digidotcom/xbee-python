017f831c92278dad0cbe2ff64b9013d46dc24412
David Escalona <david.escalona@digi.com>
2019-08-21 17:36:19 +0200

    examples: profile: add new example to apply XBee profiles
    https://jira.digi.com/browse/XBPL-217
    
    Signed-off-by: David Escalona <david.escalona@digi.com>

---
+++ examples/profile/ApplyXBeeProfileSample/ApplyXBeeProfileSample.py
@@ -0,0 +1,60 @@
+# Copyright 2019, Digi International Inc.
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from digi.xbee.devices import XBeeDevice
+from digi.xbee.profile import XBeeProfile, ReadProfileException
+
+# TODO: Replace with the location of the XBee profile file to read.
+PROFILE_PATH = "<path_to_profile>"
+# TODO: Replace with the serial port where your local module is connected to.
+PORT = "COM1"
+# TODO: Replace with the baud rate of your local module.
+BAUD_RATE = 9600
+
+
+def main():
+    print(" +----------------------------------------------+")
+    print(" | XBee Python Library Read XBee Profile Sample |")
+    print(" +----------------------------------------------+\n")
+
+    xbee_profile = None
+    try:
+        xbee_profile = XBeeProfile(PROFILE_PATH)
+    except (ValueError, ReadProfileException) as e:
+        print(str(e))
+        exit(1)
+
+    device = XBeeDevice(PORT, BAUD_RATE)
+    try:
+        device.open()
+        print("Updating profile '%s'...\n" % PROFILE_PATH)
+        device.apply_profile(xbee_profile, progress_callback=progress_callback)
+        print("\nProfile updated successfully!")
+    except Exception as e:
+        print(str(e))
+        exit(1)
+    finally:
+        if device.is_open():
+            device.close()
+
+
+def progress_callback(task, percent):
+    if percent is not None:
+        print("%s: %d%%" % (task, percent))
+    else:
+        print("%s" % task)
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/profile/ApplyXBeeProfileSample/readme.txt
@@ -0,0 +1,69 @@
+  Introduction
+  ------------
+  This sample Python application shows how to apply an existing XBee profile
+  to a XBee device.
+  
+  The application provides the profile file to the update method as well as a
+  callback function to be notified of progress.
+  
+  NOTE: This example uses the generic XBee device (XBeeDevice) class, but it 
+        can be applied to any other local device class.
+
+
+  Requirements
+  ------------
+  To run this example you will need:
+  
+    * One XBee radio in API mode and its corresponding carrier board (XBIB 
+      or XBee Development Board).
+    * The XCTU application (available at www.digi.com/xctu).
+    * The XBee profile file to update the device.
+
+
+  Compatible hardware
+  --------------------
+    * Local XBee3 devices
+
+
+  Compatible protocols
+  --------------------
+    * 802.15.4
+    * DigiMesh
+    * ZigBee
+
+
+  Example setup
+  -------------
+    1) Plug the XBee radio into the XBee adapter and connect it to your
+       computer's USB or serial port.
+       
+    2) Ensure that the module is in API mode.
+       For further information on how to perform this task, read the 
+       'Configuring Your XBee Modules' topic of the Getting Started guide.
+       
+    3) Set the port and baud rate of the XBee radio in the sample file class.
+       If you configured the module in the previous step with the XCTU, you 
+       will see the port number and baud rate in the 'Port' label of the device 
+       on the left view.
+
+    4) Configure the path of the XBee profile file in the sample file class.
+
+
+  Running the example
+  -------------------
+  First, build and launch the application. To test the functionality, check 
+  that the output console displays this message:
+  
+    "Updating profile '<profile_path>'..."
+    
+  Followed by percentage progress messages by task:
+
+    "Updating XBee firmware: 1%"
+    "Updating XBee firmware: 2%"
+    . . .
+
+  When the process completes, the following message is displayed:
+
+    "Profile updated successfully!"
+
+  If any error occurs during the process, it will be displayed in the console.
