89f7e8e4ec2331c3a2416f46d1eb5a5fcc45fd68
Tatiana Leon <tatiana.leon@digi.com>
2020-03-17 11:55:25 +0100

    update copyrights
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

--- digi/xbee/models/zdo.py
+++ digi/xbee/models/zdo.py
@@ -1,4 +1,4 @@
-# Copyright 2019, Digi International Inc.
+# Copyright 2019, 2020, Digi International Inc.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
