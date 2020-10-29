de8b49cd2ccc064bef3768cea98467a62b597365
Tatiana Leon <Tatiana.Leon@digi.com>
2020-10-29 14:54:52 +0100

    doc: fix sphinx build
    Signed-off-by: Tatiana Leon <Tatiana.Leon@digi.com>

--- doc/conf.py
+++ doc/conf.py
@@ -16,12 +16,12 @@ import sys
 import os
 import sphinx_rtd_theme
 
-from digi.xbee import __version__
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+
+from digi.xbee import __version__
 
 # -- General configuration ------------------------------------------------
 
