e8b2f369fcc1d3f392f2b60fcfebb8681c3fdb0c
Tatiana Leon <Tatiana.Leon@digi.com>
2021-03-18 14:31:30 +0100

    setup.py: set 'text/x-rst' as the long description content type
    Signed-off-by: Tatiana Leon <Tatiana.Leon@digi.com>

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     version=about['__version__'],
     description=about['__description__'],
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/x-rst',
     url=about['__url__'],
     author=about['__author__'],
     author_email=about['__author_email__'],
